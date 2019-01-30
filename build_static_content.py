9818c9991796c75041f5b96b2d3efd885dea4227
Matt Holmes <MattHolmes@ad.highlandsolutions.com>
2019-01-29 20:53:22 -0600

    adding a module to build out static html pages so that I can play around with the markup and not have to deploy it to see how it looks

---
+++ build_static_content.py
@@ -0,0 +1,12 @@
+from index import get_view
+
+def build_static_content():
+    files = ['sign_in', 'home'];
+
+    for file in files:
+        with open('static_content/' + file + '.html', 'w+') as newFile:
+            newFile.write(get_view(file))
+
+
+if __name__ == '__main__':
+    build_static_content()

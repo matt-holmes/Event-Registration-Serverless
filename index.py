7807cfadd09d8b90e1a4aaa26467d712457cc11e
Matt <matt.m.holmes@protonmail.com>
2019-02-06 21:58:14 -0600

    refactoring and adding tests

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 import json
 import hashlib
 import uuid
-import boto3
 
 def home_handler(event, context):
     return handle_request('home', event)
@@ -18,43 +17,38 @@ def handle_request(page_name, event):
         return get_view(page_name)
     elif action == 'POST':
         if page_name == 'sign_up':
-            user = create_user(event['body'])
-            return {
-                'cookie': 'X-token=' + user['session_token'],
-                'body':user,
-            }
+            return create_user(event['body'])
+
         return event
 
 def get_view(page_name):
-    with open('views/header.html') as file:
-        header = file.read()
-    with open('views/footer.html') as file:
-        footer = file.read()
-    with open('views/w3.css') as file:
-        w3_css = file.read()
-    with open('views/style.css') as file:
-        style_css = file.read()
-    with open('views/common.html') as file:
-        common = file.read()
-    with open('views/' + page_name + '.html') as file:
-        page_view = file.read()
-    with open('views/common.js') as file:
-        common_js = file.read()
-    with open('views/navigation.html') as file:
-        navigation = file.read()
-
     js_options = get_js_options(page_name)
+    view_parts = get_view_parts(page_name)
 
-    header = header.format(w3_css=w3_css, style_css=style_css)
-    return common.format(
+    header = view_parts['header.html'].format(
+        w3_css=view_parts['w3.css'],
+        style_css=view_parts['style.css']
+    )
+
+    return view_parts['common.html'].format(
         header=header,
-        view_content=page_view,
-        footer=footer,
-        common_js=common_js,
-        navigation=navigation,
+        view_content=view_parts[page_name + '.html'],
+        footer=view_parts['footer.html'],
+        common_js=view_parts['common.js'],
+        navigation=view_parts['navigation.html'],
         js_options=js_options
     )
 
+def get_view_parts(page_name):
+    view_parts = {}
+    view_item_names = ['header.html', 'footer.html', 'w3.css', 'style.css',
+                        'common.html', 'common.js', 'navigation.html']
+    view_item_names.append(page_name + '.html')
+    for item in view_item_names:
+        with open('views/' + item) as file:
+            view_parts[item] = file.read()
+
+    return view_parts
 
 def is_logged_in():
     return False
@@ -65,11 +59,19 @@ def get_js_options(page_name):
 
 def create_user(inputs):
     #TODO validate inputs
+    user = get_new_user_data(inputs)
+    response = get_table_connection('usersTable').put_item(Item=user)
+    return {
+        'cookie': 'X-token=' + user['session_token'],
+        'body':user,
+    }
+
+def get_new_user_data(inputs):
     salt = uuid.uuid4().hex
     salted_input = inputs['password'].encode('utf-8') + salt.encode('utf-8')
     hashed_password = hashlib.sha256(salted_input).hexdigest()
     id = str(uuid.uuid4())
-    user = {
+    return {
         'id' : id,
         'first_name' : inputs['first_name'],
         'last_name' : inputs['last_name'],
@@ -79,10 +81,12 @@ def create_user(inputs):
         'session_token' : id + ':' + hashed_password
     }
 
-    response = get_table_connection('usersTable').put_item(Item=user)
-    return user
-
 def get_table_connection(table_name):
+    try:
+        boto3
+    except NameError:
+        import boto3
+
     dynamodb = boto3.resource('dynamodb',
                 region_name='us-east-2',
                 endpoint_url="https://dynamodb.us-east-2.amazonaws.com")
--- tests.py
+++ tests.py
@@ -2,16 +2,42 @@ import unittest
 from index import *
 
 class TestIndex(unittest.TestCase):
+
     def test_home_handler(self):
         event = {'method' : 'GET'}
         context = {}
         result = home_handler(event, context)
         self.assertNotEqual(result.find("Home"), -1)
+
     def test_sign_in_handler(self):
         event = {'method' : 'GET'}
         context = {}
         result = sign_in_handler(event, context)
         self.assertNotEqual(result.find("Sign In"), -1)
+
+    def test_get_new_user_data(self):
+        inputs = {
+            'first_name' : 'Jon',
+            'last_name' : 'Green',
+            'email' : 'jgreen@test.com',
+            'username' : 'jgreen',
+            'password' : 'test1234',
+            'confirm_password' : 'test1234'
+        }
+
+        result = get_new_user_data(inputs)
+        self.assertEqual(result['first_name'], 'Jon')
+        self.assertEqual(result['last_name'], 'Green')
+        self.assertEqual(result['email'], 'jgreen@test.com')
+        self.assertEqual(result['username'], 'jgreen')
+        self.assertTrue(result['id'])
+        self.assertTrue(result['password'])
+        self.assertTrue(result['session_token'])
+        with self.assertRaises(KeyError):
+            result['confirm_password']
+
+
+
 
 if __name__ == '__main__':
     unittest.main()

21bb3a7e5f37527931eb02ac92abd742f665a969
Matt <matt.m.holmes@protonmail.com>
2019-02-19 20:23:09 -0600

    adding some more tests so that this isn't so damn pathetic

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import unittest
 from index import *
 from request_helpers import *
+from models import User
 
 class TestIndex(unittest.TestCase):
 
@@ -37,8 +38,32 @@ class TestIndex(unittest.TestCase):
         self.assertTrue(result['id'])
         self.assertTrue(result['password'])
         self.assertTrue(result['session_token'])
+        self.assertEqual(result['id'], result['session_token'].split(':')[0])
         with self.assertRaises(KeyError):
             result['confirm_password']
+
+    def test_get_and_set_user_attributes(self):
+        attributes = {'test_1': 'test'}
+        user = User(attributes)
+        self.assertEqual('test', user.get('test_1'))
+        user.set_attribute('test_1', 'test2')
+        self.assertEqual('test2', user.get('test_1'))
+        self.assertEqual('test2', user.get_attributes()['test_1'])
+        attributes = {'test_1': 'test3'}
+        user.set_attributes(attributes)
+        self.assertEqual('test3', user.get('test_1'))
+        self.assertEqual(None, user.get('test_3'))
+
+    def test_user_check_password(self):
+        user = User()
+        user.set('password', '1143a939b4b13fd5088b469f0789bfab2194dabf2a0c219fb7bb9b48479d8279:a7072da7e8b042a683732046b01d1208')
+        self.assertTrue(user.check_password('test1234'))
+        self.assertFalse(user.check_password('test12345'))
+
+    def test_get_hashed_password(self):
+        self.assertEqual(1, len(get_hashed_password('test').split(':')))
+        self.assertEqual(2, len(get_hashed_password('test', True).split(':')))
+
 
 
 
